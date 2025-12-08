e0651fb500a48c833c0e4bcda3220c2af26971c0
said.chaabane <webhost48@gmail.com>
2025-12-08 23:16:38 +0100

    clarification comment

--- services/base-api-client.ts
+++ services/base-api-client.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+// remark : this is a very basic implementation generally here we will have header and other configurations , interceptors for refresh tokens ...
+
 export const baseApiClient = axios.create({
   // this should be from env
   baseURL: "https://restcountries.com/v3.1",
